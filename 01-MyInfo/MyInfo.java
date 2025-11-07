04f9c9afee1d06cdf12b6b6a98f36b27df011252
Bambam <punyanuch.ksd@gmail.com>
2025-11-07 09:08:01 +0700

    Add 01-MyInfo: Basic Java program displaying personal information

---
+++ 01-MyInfo/MyInfo.java
@@ -0,0 +1,11 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        String name ="Jonh Doe";
+        int age = 22;
+        String hobby = "Gaming";
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old.");
+        System.out.println("My hobby is " + hobby + ".");
+    }
+}
